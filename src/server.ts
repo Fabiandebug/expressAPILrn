4d683f100db9ee983060b51c372ad92e6d101fde
Maina Evans <kinuthiafabiankinuthia@gmail.com>
2023-01-10 01:50:53 +0300

    Using cors

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,25 @@ import bodyParser from 'body-parser'
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
 
+const corsOptions = {
+    origin: "https://somedomain.co.ke",
+    optionsSuccessStatus: 200
+}
+
+
+// This uses cors for the whole app routes
+app.use(cors(corsOptions))
+//  
 app.use(bodyParser.json())
 
 app.get('/', function (req: Request, res: Response) {
     res.send("Hello there server is running")
 });
+
+// This uses cors for specfic routes
+app.get('/test-corse', cors(corsOptions), function (_req, res, next) {
+    res.json({ msg: "This is a corse enebled route" })
+})
 
 app.listen(3000, function () {
     console.log(`Server is Running on:${address}`)
